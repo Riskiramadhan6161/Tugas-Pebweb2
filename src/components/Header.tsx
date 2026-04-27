766f99fa60cb2fe1a624a670f339d9f02f5d5f43
unknown <rizkiramdhanrizki36484@gmail.com>
2026-04-27 11:39:35 +0700

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,23 @@
 import { Home, Info, Users, HelpCircle } from "lucide-react";
-import { NavLink } from "./tampilan/NavLink";
+import { NavLink } from "./ui/NavLink";
+import { useLocation, Link } from "react-router-dom";
 
-export const Header: React.FC = () => {
-  const currentPath = "#";
+export default function Header() {
+  const location = useLocation();
 
   const menuItems = [
-    { label: "Beranda", href: "#", icon: <Home size={18} /> },
-    { label: "Tentang", href: "#about", icon: <Info size={18} /> },
-    { label: "Narasumber", href: "#speakers", icon: <Users size={18} /> },
-    { label: "FAQ", href: "#faq", icon: <HelpCircle size={18} /> },
+    { label: "Beranda", href: "/", icon: <Home size={18} /> },
+    { label: "Competition", href: "/competition", icon: <Info size={18} /> },
+    { label: "Seminar", href: "/seminar", icon: <Users size={18} /> },
+    { label: "Workshop", href: "/workshop", icon: <HelpCircle size={18} /> },
+    { label: "Talkshow", href: "/talkshow", icon: <HelpCircle size={18} /> },
   ];
 
   return (
-    <header className=" bg-white shadow-sm px-6 py-2">
-      <div className="max-w-7xl mx-auto flex justify-between flex-center gap-4">
+    <header className="bg-white shadow-sm px-12 py-2 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto flex justify-between items-center gap-4">
+        
+        {/* Logo */}
         <div className="logo">
           <img
             src="https://www.invofest-harkatnegeri.com/assets/nav-logo.png"
@@ -21,21 +25,29 @@ export const Header: React.FC = () => {
             className="h-16"
           />
         </div>
-        <div className="nav flex gap-4">
+
+        {/* Menu */}
+        <div className="flex items-center gap-4">
           {menuItems.map((item) => (
             <NavLink
               key={item.label}
               label={item.label}
               href={item.href}
               icon={item.icon}
-              // Logika: Jika href sama dengan path saat ini, maka Active
-              isActive={item.href === currentPath}
+              isActive={location.pathname === item.href}
             />
           ))}
         </div>
+
+        {/* LOGIN BUTTON */}
+        <Link
+          to="/login"
+          className="bg-red-900 text-white px-4 py-2 rounded-lg hover:bg-red-800 transition"
+        >
+          Login
+        </Link>
+
       </div>
     </header>
   );
-};
-
-export default Header;
+}
